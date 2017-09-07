35f482057c0081974a37cf04504329ab7a201560
gilemon <gilemon@gmail.com>
2017-09-07 11:01:47 +0200

    Add the files

---
+++ conf.js
@@ -0,0 +1,13 @@
+var cryptoArray = [	{"name":"BITCOIN", "symbol":"btc", "refVal":898, "refDate":"12/2013"},
+					{"name":"LITECOIN", "symbol":"ltc", "refVal":23, "refDate":"12/2014"},
+					{"name":"DASH", "symbol":"dash", "refVal":15.7, "refDate":"12/2014"},
+					{"name":"ETHEREUM", "symbol":"eth", "refVal":41.2, "refDate":"04/2017"},
+					{"name":"Ripple", "symbol":"xrp", "refVal":0.032, "refDate":"04/2017"},
+					{"name":"DigiByte", "symbol":"dgb", "refVal":0.001134635, "refDate":"04/2017"},
+					{"name":"ReddCoin", "symbol":"rdd", "refVal":0.0006, "refDate":"05/2017"},
+					{"name":"Zcash", "symbol":"zec", "refVal":227, "refDate":"05/2017"},
+					{"name":"SIA", "symbol":"sc", "refVal":0.0000001, "refDate":"07/2017"},
+					{"name":"BITCOIN CASH", "symbol":"bch", "refVal":256, "refDate":"08/2017"}
+				];
+				
+var  API_URL = "https://api.cryptonator.com/api/ticker/";
