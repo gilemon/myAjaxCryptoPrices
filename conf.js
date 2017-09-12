acd8a52467ad31c00dfbf15e1a5c99cecfce5a01
gilemon <1597000+gilemon@users.noreply.github.com>
2017-09-12 12:45:29 +0200

    add icons for know coins

--- conf.js
+++ conf.js
@@ -1,20 +1,13 @@
-/*var cryptoArray = [	{"name":"BITCOIN", "symbol":"btc", "refVal":898, "refDate":"12/2013"},
+var cryptoArray = [	{"name":"BITCOIN", "symbol":"btc", "refVal":898, "refDate":"12/2013"},
 					{"name":"LITECOIN", "symbol":"ltc", "refVal":23, "refDate":"12/2014"},
 					{"name":"DASH", "symbol":"dash", "refVal":15.7, "refDate":"12/2014"},
 					{"name":"ETHEREUM", "symbol":"eth", "refVal":41.2, "refDate":"04/2017"},
 					{"name":"Ripple", "symbol":"xrp", "refVal":0.032, "refDate":"04/2017"},
 					{"name":"DigiByte", "symbol":"dgb", "refVal":0.001134635, "refDate":"04/2017"},
 					{"name":"ReddCoin", "symbol":"rdd", "refVal":0.0006, "refDate":"05/2017"},
-					{"name":"Zcash", "symbol":"zec", "refVal":227, "refDate":"05/2017"},
-					{"name":"SIA", "symbol":"sc", "refVal":0.0000001, "refDate":"07/2017"},
-					{"name":"BITCOIN CASH", "symbol":"bch", "refVal":256, "refDate":"08/2017"}
-				];*/
-				
-				
-var cryptoArray = [	{"name":"EOS", "symbol":"eos", "refVal":0.72, "refDate":"08/2017"},
-					{"name":"IOTA", "symbol":"iot", "refVal":0.58, "refDate":"08/2017"}
+					{"name":"Zcash", "symbol":"zec", "refVal":227, "refDate":"05/2017"}
 					];
 					
-var currencyCompare = {"name":"EURO", "symbol":"eur"};
+var currencyCompare = {"name":"&euro;", "symbol":"eur"};
 				
 var  API_URL = "https://api.cryptonator.com/api/ticker/";
