b70b591c939debf51dc4f67f298cbc2b6bed9bcd
gilemon <1597000+gilemon@users.noreply.github.com>
2017-09-15 17:24:53 +0200

    multiple API feeds
    support average from multiple API feeds (added cryptocompare)

--- conf.js
+++ conf.js
@@ -4,10 +4,10 @@ var cryptoArray = [	{"name":"BITCOIN", "symbol":"btc", "refVal":898, "refDate":"
 					{"name":"ETHEREUM", "symbol":"eth", "refVal":41.2, "refDate":"04/2017"},
 					{"name":"Ripple", "symbol":"xrp", "refVal":0.032, "refDate":"04/2017"},
 					{"name":"DigiByte", "symbol":"dgb", "refVal":0.001134635, "refDate":"04/2017"},
-					{"name":"ReddCoin", "symbol":"rdd", "refVal":0.0006, "refDate":"05/2017"},
-					{"name":"Zcash", "symbol":"zec", "refVal":227, "refDate":"05/2017"}
+					{"name":"ReddCoin", "symbol":"rdd", "refVal":0.0006, "refDate":"05/2017"}
 					];
-					
+
 var currencyCompare = {"name":"&euro;", "symbol":"eur"};
 				
-var  API_URL = "https://api.cryptonator.com/api/ticker/";
+var  API_URL = [	{"url":"https://min-api.cryptocompare.com/data/price?fsym=$from&tsyms=$to", "priceJSONPath":"compareCurrencyUpper"},
+					{"url":"https://api.cryptonator.com/api/ticker/$from-$to", "priceJSONPath":"ticker.price"}];
